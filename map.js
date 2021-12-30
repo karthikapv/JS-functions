1f60bdc4a2d3fe0dc151f90c4dc8a0b50e8396de
karthikapv <karthikapv311@gmail.com>
2021-12-30 21:58:09 +0530

    minor changes

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 const map = (arr, callback) => {
 let index = 0;
-const resultArray = [];
+
 const mapFunction = (arr, callback, index) => {
     if(index==arr.length) return arr;
         arr[index] = callback(arr[index]);
