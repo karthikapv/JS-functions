daaf47484841d43bf94c8dcd4d8ef78df6d11c7c
karthikapv <karthikapv311@gmail.com>
2021-12-31 18:48:51 +0530

    test added

--- map.js
+++ map.js
@@ -1,12 +1,18 @@
 const map = (arr, callback) => {
-let index = 0;
-
+let index = 0; 
 const mapFunction = (arr, callback, index) => {
     if(index==arr.length) return arr;
         arr[index] = callback(arr[index]);
     return mapFunction(arr, callback, index+1);
 }
-
 return mapFunction(arr, callback, index);
 }
 
+//code to test map function 
+
+const arr = [1,2,3,4,5];
+function addTen(num){
+    return (num+10);
+}
+
+console.log(map(arr,addTen)); 
